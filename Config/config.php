11c498b5be0242aec318163a3f20982db0431c7e
unknown <jorgesosa@fundaciondevalores.org>
2023-12-06 11:29:52 -0400

    initial commit

---
+++ Config/config.php
@@ -0,0 +1,8 @@
+<?php
+
+define('HOST_NAME', 'localhost');
+define('USER_NAME', 'root');
+define('PASSWORD', '');
+define('DB_NAME', 'roles');
+ 
+
---
+++ Models/Database.php
@@ -0,0 +1,32 @@
+<?php
+require_once '../Config/config.php';
+
+class Database
+{
+    private $hostname;
+    private $username;
+    private $password;
+    private $dbname;
+    private $conn;
+    
+    public function __construct()
+    {
+        $this->hostname = HOST_NAME;
+        $this->username = USER_NAME;
+        $this->password = PASSWORD;
+        $this->dbname = DB_NAME;
+
+        $this->conn = new mysqli($this->hostname, $this->username, $this->password, $this->dbname);
+    }
+
+
+    function getConn()
+    {
+        return $this->conn;
+    }
+
+    function closeConn()
+    {
+        $this->conn->close();
+    }
+}
